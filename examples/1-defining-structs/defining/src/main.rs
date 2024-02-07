cf30c6876b2cbfaa7cb169465eefa4cb6dd10f38
rbbozkurt <resit.b.bozkuXrt@campus.tu-berlin.de>
2024-02-07 16:31:21 +0100

    person impl func added
    Signed-off-by: rbbozkurt <resit.b.bozkuXrt@campus.tu-berlin.de>

--- examples/1-defining-structs/defining/src/main.rs
+++ examples/1-defining-structs/defining/src/main.rs
@@ -3,13 +3,24 @@
 struct Person {
     first_name: String,
     last_name: String,
+    email: String,
+    phone_number: String,
     age: u8,
+}
+
+impl Person {
+    fn full_name(&self) -> String {
+        format!("{} {}", self.first_name, self.last_name)
+    }
 }
 
 fn main() {
     println!("{:?}", Person {
-        first_name: "John".to_string(),
-        last_name: "Doe".to_string(),
-        age: 25,
-    });
+            first_name: "John".to_string(),
+            last_name: "Doe".to_string(),
+            age: 25,
+            email: "john-doe@foo.com".to_string(),
+            phone_number: "123-456-7890".to_string(),
+        }.full_name()
+    );
 }
